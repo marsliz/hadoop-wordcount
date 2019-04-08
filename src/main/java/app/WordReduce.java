1d296e590c707d21974b61f26fb973c7f6f5740d
Faruk Zeren <28390104+marsliz@users.noreply.github.com>
2019-04-08 16:47:58 +0300

    Update WordReduce.java

--- src/main/java/app/WordReduce.java
+++ src/main/java/app/WordReduce.java
@@ -12,10 +12,10 @@ public class WordReduce extends Reducer<Text, IntWritable, Text, IntWritable> {
 	@Override
 	public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
         int sum = 0;
-		for (IntWritable val : values) {
-			sum += val.get();
-		}
-		result.set(sum);
-		context.write(key, result);
+	for (IntWritable val : values) {
+		sum += val.get();
+	}
+	result.set(sum);
+	context.write(key, result);
     }
 }
