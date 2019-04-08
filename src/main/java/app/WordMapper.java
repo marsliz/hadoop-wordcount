79c0eac82439059138ff6e590d1b0744269a1d07
Faruk Zeren <28390104+marsliz@users.noreply.github.com>
2019-04-08 16:47:21 +0300

    Update WordMapper.java

--- src/main/java/app/WordMapper.java
+++ src/main/java/app/WordMapper.java
@@ -14,10 +14,10 @@ public class WordMapper extends Mapper<Object, Text, Text, IntWritable> {
 	@Override
 	public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
         String line = value.toString();
-		StringTokenizer tokenizer = new StringTokenizer(line);
-		while (tokenizer.hasMoreTokens()) {
-			word.set(tokenizer.nextToken());
-			context.write(word, one);
-		}
+	StringTokenizer tokenizer = new StringTokenizer(line);
+	while (tokenizer.hasMoreTokens()) {
+		word.set(tokenizer.nextToken());
+		context.write(word, one);
+	}
     }
 }
