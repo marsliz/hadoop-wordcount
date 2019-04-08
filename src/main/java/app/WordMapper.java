e93ab04a253b98316c32f462464cf88d9edc1ed4
Faruk Zeren <28390104+marsliz@users.noreply.github.com>
2019-04-08 12:08:39 +0300

    Add files via upload

---
+++ src/main/java/app/WordMapper.java
@@ -0,0 +1,23 @@
+package app;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WordMapper extends Mapper<Object, Text, Text, IntWritable> {
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+	
+	@Override
+	public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+		StringTokenizer tokenizer = new StringTokenizer(line);
+		while (tokenizer.hasMoreTokens()) {
+			word.set(tokenizer.nextToken());
+			context.write(word, one);
+		}
+    }
+}
